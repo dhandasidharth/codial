9d60c13c54b4d6b039182b11b90d0fd98b76932b
Sidharth Singh Dhanda <sidharthsinghdhanda@gmail.com>
2022-12-15 01:00:47 +0530

    setup layout

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const port = 8000;
 const app = express();
-
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+app.use(express.static('./assets'));
 app.use('/',require('./routes/index'));
 app.set('view engine',"ejs");
 app.set('views','./views');
-
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 
 
