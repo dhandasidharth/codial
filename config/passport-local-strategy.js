357066f208f057b0c299d4f7384816d83ad057ea
Sidharth Singh Dhanda <sidharthsinghdhanda@gmail.com>
2022-12-24 10:40:55 +0530

    added passport.js

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,34 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
+passport.use(new LocalStrategy({
+    usernameField:'email'
+    },
+    function(email,password,done){
+        User.findOne({email:email},function(err,user){
+            if (err){
+                console.log("error inside local strategy");
+                return done(err);
+            }
+            if (!user || user.password != password){
+                console.log("invalid input data");
+                return done(null,false);
+            }
+            return done(null,user);
+        })
+    }
+))
+
+passport.serializeUser(function(user,done){
+    done(null,user.id);
+})
+passport.deserializeUser(function(id,done){
+    User.findById(id,function(err,user){
+        if (err){
+            console.log('error in deserialising user');
+            return done(err);
+        }
+        return done(null,user);
+    })
+})
+module.exports = passport;
