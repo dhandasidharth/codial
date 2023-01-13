1626e305f463c264492cef9717b69571bbc84c0f
Sidharth Singh Dhanda <sidharthsinghdhanda@gmail.com>
2023-01-13 01:01:33 -0600

    added controller and router for posting

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const home_control = require('../controller/first_control');
 router.get('/',home_control.home);
 router.use('/users',require('./users'));
+router.use('/posts',require('./posts'));
 console.log("Router is up");
 module.exports = router;
