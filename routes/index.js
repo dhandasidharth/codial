31979ba7f5b113dfced79efafe899ab201a4aa64
Sidharth Singh Dhanda <sidharthsinghdhanda@gmail.com>
2023-01-30 19:30:22 -0600

    populated comments in post & user in comment. Further, added comment HTML code in ejs file

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const home_control = require('../controller/first_control');
 router.get('/',home_control.home);
 router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
+router.use('/comments',require('./comments'));
 console.log("Router is up");
 module.exports = router;
